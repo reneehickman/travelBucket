2e4e8e5afdfafa2f6267601a85ffb0083c5c7ba7
Renee Hickman <renee.hickman@outlook.com>
2019-01-29 16:13:22 -0600

    html base

---
+++ assets/javascript/script.js
@@ -0,0 +1,16 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAacNRFRGzgHM8W0blNJyjBtMEZ23nkWpQ",
+    authDomain: "travelbucket-f12cf.firebaseapp.com",
+    databaseURL: "https://travelbucket-f12cf.firebaseio.com",
+    projectId: "travelbucket-f12cf",
+    storageBucket: "travelbucket-f12cf.appspot.com",
+    messagingSenderId: "1042574383776"
+  };
+  firebase.initializeApp(config);
+
+
+
+  var tmURL = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=bAS6qJ9GjnzpjGB3vGbEXGpiwlpn1ppZ';
+
+  var googlePlacesURL = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=PHOTOREF&key=AIzaSyCSGl6uFr0JoT1c9RHRu369r_hdYj0eknA';
