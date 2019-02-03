b960c1852cb0cd0fef4641aaac85742190360b6e
reneecosmic <renee.hickman@outlook.com>
2019-02-03 15:53:19 -0600

    updated styling + onload js state

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,49 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyAacNRFRGzgHM8W0blNJyjBtMEZ23nkWpQ",
-    authDomain: "travelbucket-f12cf.firebaseapp.com",
-    databaseURL: "https://travelbucket-f12cf.firebaseio.com",
-    projectId: "travelbucket-f12cf",
-    storageBucket: "travelbucket-f12cf.appspot.com",
-    messagingSenderId: "1042574383776"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyAacNRFRGzgHM8W0blNJyjBtMEZ23nkWpQ",
+  authDomain: "travelbucket-f12cf.firebaseapp.com",
+  databaseURL: "https://travelbucket-f12cf.firebaseio.com",
+  projectId: "travelbucket-f12cf",
+  storageBucket: "travelbucket-f12cf.appspot.com",
+  messagingSenderId: "1042574383776"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
+
+var initialSearchDIV = $("#initialSearchBodyContent");
+var resultsDIV = $("#resultsDIV");
+var bucketListDIV = $("#bucketListDIV");
+var loginDIV = $("#loginDIV");
+var loginButton = $("#login");
+var loggedInButton = $("#loggedIn");
+var addToBucketListButton = $("#addToBucketListLink");
+var removeFromBucketListButton = $("#RemoveFromBucketListLink");
 
 
-  //var tmURL = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=bAS6qJ9GjnzpjGB3vGbEXGpiwlpn1ppZ';
+  initialSearchDIV.show();
+  resultsDIV.hide();
+  bucketListDIV.hide();
+  loginDIV.hide();
+  loginButton.show();
+  loggedInButton.hide();
+  addToBucketListButton.hide();
+  removeFromBucketListButton.hide();
 
-  //var googlePlacesURL = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=PHOTOREF&key=AIzaSyCSGl6uFr0JoT1c9RHRu369r_hdYj0eknA';
 
-  //AccuWeather: TniUBmLQMyMOCQn9JQeefxCVZEbpTolr
+// function showResults(){
+//   
+// }
+
+
+
+//var tmURL = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=bAS6qJ9GjnzpjGB3vGbEXGpiwlpn1ppZ';
+
+//var googlePlacesURL = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=PHOTOREF&key=AIzaSyCSGl6uFr0JoT1c9RHRu369r_hdYj0eknA';
+
+//AccuWeather: TniUBmLQMyMOCQn9JQeefxCVZEbpTolr
+
+// $("#initialSearchButton").on("Click", showResults);
+
+// var initialSearchButton = $("#initialSearchButton");
+// initialSearchButton.on("click", showResults);
