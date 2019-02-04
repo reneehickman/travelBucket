7b0896935a1d36ae65d35209662942ff344b7c10
reneecosmic <renee.hickman@outlook.com>
2019-02-03 23:38:08 -0600

    updated css

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -31,10 +31,43 @@ var removeFromBucketListButton = $("#RemoveFromBucketListLink");
   initialSearchDIV.show();
   loginButton.show();
 
+  
 
-// function showResults(){
-//   
-// }
+  function showResults(){
+    initialSearchDIV.hide();
+    resultsDIV.show()
+    addToBucketListButton.show();
+  };
+
+
+  // function checkAuthentication(){
+  //   //check if user is logged in
+  //   //if user is logged in show users bucket list
+  //   bucketListDIV.show();
+  //   //show users name button instead of sign in sign up button
+  //   loggedInButton.show();
+  //   loginButton.hide();
+  //   //if user is not logged in, prompt user to login via modal
+  //   //if no account, prompt to sign up
+  // };
+
+  // function addToBucketList(){
+  //   // checkAuthentication();
+  //   // function that pushes the city the user is saving into bucket list array
+  //   //display in users bucket list
+  // };
+
+
+  $(".searchButton").on("click", showResults);
+  // $("#addToBucketListButton").on("click", addToBucketList);
+
+  // $(".searchButton").on("click", function(){
+  //   initialSearchDIV.hide();
+  //   resultsDIV.show()
+  // });
+
+  
+
 
 
 
@@ -43,8 +76,3 @@ var removeFromBucketListButton = $("#RemoveFromBucketListLink");
 //var googlePlacesURL = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=PHOTOREF&key=AIzaSyCSGl6uFr0JoT1c9RHRu369r_hdYj0eknA';
 
 //AccuWeather: TniUBmLQMyMOCQn9JQeefxCVZEbpTolr
-
-// $("#initialSearchButton").on("Click", showResults);
-
-// var initialSearchButton = $("#initialSearchButton");
-// initialSearchButton.on("click", showResults);
